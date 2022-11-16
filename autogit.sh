680b3fdc0d6ab2d300bfc72912be3e276214fff9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-11-15 22:41:30 -0300

    git remote fixed

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "Project inserted"
+git commit -m "git remote fixed"
 git branch -M main
 git remote add origin https://github.com/CarlosViniMSouza/challenge05-nodejs.git
 git push -u origin main
